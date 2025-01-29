5094ba1e6d813de5be9aa7567fe0864bf99aea61
Gabriel Zério <21043296+gabrielzerio@users.noreply.github.com>
2025-01-29 17:37:34 -0300

    back-end 'API' lista-compras

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import rotasPublicas from './rotas/publica.js';
+import rotasPrivadas from './rotas/privada.js';
+
+import auth from './middlewares/auth.js';
+import cors from 'cors'
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/', rotasPublicas);
+app.use('/', auth, rotasPrivadas);
+
+app.listen(3000, () => console.log("Servidor Rodando 🚀"));
